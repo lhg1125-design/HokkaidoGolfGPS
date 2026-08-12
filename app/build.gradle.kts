3efa760d8f98c359741abcc3cb0e8373d9bb1fab
lhg1125-design <lhg1125@gmail.com>
2026-08-12 09:48:45 +0900

    build: configure Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.hokkaidogolf.trip"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.hokkaidogolf.trip"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "0.3.0-cloud-draft"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
